ebd2802b47d79610d0876ae788fe3ac4b6388f01
kezlevm <mkezlev@yahoo.com>
2023-03-29 01:54:29 -0400

    Added security headers

--- service/__init__.py
+++ service/__init__.py
@@ -8,10 +8,14 @@ import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
+from flask_talisman import Talisman
 
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
+
+# Create instance of Talisman
+talisman = Talisman(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
@@ -26,6 +30,8 @@ log_handlers.init_logging(app, "gunicorn.error")
 app.logger.info(70 * "*")
 app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "*")
+
+
 
 try:
     models.init_db(app)  # make our database tables
